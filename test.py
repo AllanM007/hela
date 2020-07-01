ed427f852f55c1bef1ca0308d1095cb2e615f049
AllanM007 <mwaranguallan345@gmail.com>
2020-07-01 14:55:58 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import camelot
+tables = camelot.read_pdf('Mpesa.pdf', pages='1-6')
+tables
+
+tables.export('mpesa.xlsx', f='xlsx', compress=True) # json, excel, html, sqlite
+tables[1]
+
+tables[1].parsing_report
+{
+    'accuracy': 99.02,
+    'whitespace': 12.24,
+    'order': 1,
+    'page': 1
+}
+tables[1].to_excel('mpesa.xlsx') # to_json, to_excel, to_html, to_sqlite
+tables[1].df # get a pandas DataFrame!
