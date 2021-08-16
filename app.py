ba66c7583acb5b16922f998202d2cdf400adddb8
mzawadi <mwaranguallan345@gmail.com>
2021-08-16 10:36:37 +0300

    update app.py

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ db.create_all()
 migrate = Migrate(app,db)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5050, debug=True)
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
